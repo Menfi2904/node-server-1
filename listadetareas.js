089d272e14e313ca2cbe194539fdfac550ec2bb7
Menfi2904 <menfigarcia31@gmail.com>
2023-09-08 14:25:07 -0600

    node server listatareas

--- listadetareas.js
+++ listadetareas.js
@@ -1,96 +1,39 @@
-const listaTareas = [
+const http = require("http");
+
+const tareas = [
   {
-    indicador: "tarea1",
-    descripcionDeTarea: "hacer tarea de matematicas",
-    tareaCompletada: false,
+    id: 1,
+    descripcion: "hacer compras",
+    completada: true,
   },
   {
-    indicador: "tarea2",
-    descripcionDeTarea: "hacer tarea de programacion",
-    tareaCompletada: false,
+    id: 2,
+    descripcion: "hacer tarea de fisica",
+    completada: true,
   },
   {
-    indicador: "tarea3",
-    descripcionDeTarea: "Ir a comprar al supermercado",
-    tareaCompletada: false,
+    id: 3,
+    descripcion: "hacer tarea de estadistica",
+    pendiente: false,
   },
 ];
 
+const puerto = 3000;
+const host = "localhost";
 
-async function crearNuevaTarea(indicador, descripcionTarea) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const agregarNuevaTarea = {
-        indicador: indicador,
-        descripcionDeTarea: descripcionTarea,
-        tareaCompletada: false,
-      };
-      listaTareas.push(agregarNuevaTarea);
-      resolve(agregarNuevaTarea);
-    }, 1000);
-  });
-}
-
-
-async function tareaCompletada(indicadortarea) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const tarea = listaTareas.find(
-        (tarea) => tarea.indicador === indicadortarea
-      );
-      if (!tarea) {
-        reject(new Error("tarea no encontrada"));
-      } else {
-        tarea.tareaCompletada = true;
-        resolve(tarea);
-      }
-    }, 3000);
-  });
-}
-
-
-async function borrarTareas(indicadorTarea) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const tareaIndex = listaTareas.findIndex(
-        (tareas) => tareas.indicador === indicadorTarea
-      );
-      if (tareaIndex === -1) {
-        reject(new Error("tarea no encontrada"));
-      } else {
-        const borrarTarea = listaTareas.splice(tareaIndex, 1)[0];
-        resolve(borrarTarea);
-      }
-    }, 5000);
-  });
-}
-
-async function guardarListaDeTareas() {
-  try {
-    await new Promise((resolve) => setTimeout(resolve, 7000));
-    console.log(`la lista de tareas se guardo con los siguientes datos ⮕`);
-    console.log(listaTareas);
-  } catch (error) {
-    console.log(`${error} no se puedo guardar la lista`);
+let server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/listadetareas") {
+    res.statusCode = 200;
+    res.setHeader("Content-type", "application/json");
+    res.end(JSON.stringify(tareas));
+  } else {
+    req.statusCode = 404;
+    res.end = "url no encontrada";
   }
-}
-
-crearNuevaTarea("tarea4", "Hacer la tarea de física").then((nuevaTarea) => {
-  console.log("Nueva tarea creada:", nuevaTarea);
-  return tareaCompletada(nuevaTarea.indicador);
 });
-tareaCompletada("tarea4").then((estado) => {
-  console.log("Tarea completada:", estado);
-  return tareaCompletada.indicador;
+
+server.listen(puerto, host, () => {
+  console.log(
+    `el servidor arranco adecuadamente http://localhost:${puerto}/listadetareas`
+  );
 });
-borrarTareas("tarea1")
-  .then((tareaEliminada) => {
-    console.log("Tarea eliminada:", tareaEliminada);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
-
-
-
-guardarListaDeTareas();
